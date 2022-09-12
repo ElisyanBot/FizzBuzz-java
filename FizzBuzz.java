4282415cb484fcb2f33b8c0febfd06711029835c
Marcus Larsson <Marcus.b.Larsson@outlook.com>
2022-09-12 19:11:26 +0200

    first commit

---
+++ FizzBuzz.java
@@ -0,0 +1,24 @@
+public class FizzBuzz {
+
+  public static void main(String[] args) {
+
+    for (int i = 0; i <= 100; i++) {
+
+      if(i == 42) {
+        System.out.println("Answer to the Ultimate Question of Life, the Universe, andEverything");
+      }
+      else if (i % 3 == 0 && i % 5 == 0) {
+        System.out.println("index: " + i  + " :fizzbuzz");
+      } 
+      else if (i % 3 == 0) {
+        System.out.println("index: " + i + " :fizz");
+      } 
+      else if (i % 5 == 0) {
+        System.out.println("index: " + i + " :buzz");
+      } 
+      else {
+        System.out.println("index: " + i);
+      }
+     }
+  }
+}
